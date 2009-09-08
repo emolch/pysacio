40e6a3cd2d92f3ec6ee6267520eb5e722dbbcedf
emolch <sebastian.heimann@zmaw.de>
2009-09-08 19:17:07 +0200

    removed unnecessary stuff in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
-import numpy
-from distutils.core import setup, Extension
+from distutils.core import setup
                       
 setup (name = 'pysacio',
-       include_dirs = [ numpy.get_include() ],
        version = '0.1',
        description = 'Read and write SAC files from Python',
        py_modules  = [ 'pysacio' ],
